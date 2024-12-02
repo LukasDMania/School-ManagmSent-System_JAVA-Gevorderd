642dbebc57234126caec6f84970f5e88dbec3e93
LukasDMania <lukasdestuyver2@gmail.com>
2024-12-02 14:27:35 +0100

    Spring Initializer setup

---
+++ campusmanagment/src/main/java/com/campusmanagment/CampusmanagmentApplication.java
@@ -0,0 +1,13 @@
+package com.campusmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusmanagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusmanagmentApplication.class, args);
+	}
+
+}
---
+++ campusmanagment/src/test/java/com/campusmanagment/CampusmanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusmanagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
